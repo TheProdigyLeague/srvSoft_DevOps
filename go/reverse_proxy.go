46b365b99987ca91bd723c0e87c7ac6c53d94c7d
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-07-07 19:57:05 -0500

    :waxing_gibbous_moon:

---
+++ go/reverse_proxy.go
@@ -0,0 +1,93 @@
+!/usr/bin/bridge.bat
+//                                              ......                                                
+//                                            ............                                            
+//                                     ......:------------:......                                     
+//                                     ..:-------==++===------:...                                    
+//                                 ....:----++==-:::::-+**//+=---:...                                  
+//                                 ..:---=+-..:::::........=//*=---:..                                 
+//                              ....----==..::-+*//%%%%//*+::::+//=----..                                
+//                              ...-----+:.::////=-------=+//=:--**-----.                                
+//                              ..-----+=...%*-----------=//=--+%=-----....                            
+//                              .------*+..://=------------////--=//=------..                             
+//                              .------//*.:-//-------------*%:-=//=------..                             
+//                              :------//*.:-//-------------*%::=//=------:..                            
+//                              -------//*.:=//-------------*%.:=//=------:..                            
+//                              :----+****************************+----:..                            
+//                              .--=*++==+++++++++++++++++++++++==*+---..                             
+//                              .--*+=-:--========++++++++++++++==+*=--..                             
+//                              ..:-==:-++++++++++++---:-+++++-:-.-*=-....                            
+//                         ..-%@@@@//=.--++++++++++++:%@+:+++++-//@=:*=..   .--.                        
+//                         .@@*....=+...:=:..++-..=:.%@+:-..=-.//@=::..  .::...                        
+//                        .%@*       .//%%*%@@..//@%////@@@+.*@%////@@@=-@=. .=@-                           
+//                         %@:.      ..----%@*+@+....%@+=@=.. .//@=.*@:.:@+.                           
+//                         +@%:.  ....@@+==%@*+@+. ..%@+=@+. ..//@=.:%@.@//:                            
+//                          -@@////////@%-@@*=*@@*.//@//////%@@+.*@//////%@@=  -@@%:                             
+//                           ..:--:.. .::::.:.  .:-:.::. ..:-:.::....%@=.                             
+//                                                               .@@@@+                               
+//                                                                ...                                 
+//
+https://github.com/TheProdigyLeague/Xkeys_Network
+https://contrib.rocks/image?repo=OWASP/Nettacker
+//      _ _            _   
+//  ___| (_) ___ _ __ | |_ 
+// / __| | |/ _ \ '_ \| __|
+//| (__| | |  __/ | | | |_ 
+// \___|_|_|\___|_| |_|\__|
+//
+{$2y$04$6scGRBnxIlEO7my3RCWdZ.B8/XaLVdY0Ux9YkTGK3GgPYVHNG4ZGi}='<🐺🍫  𝓾𝕤ｅｒ Ⓐς𝕥𝓲𝐎𝕟  👍👮>'
+{$2y$04$9UqHPRp298Mj6mk6mRVo1eJIOJ.h5icr4JCp5GsdRyqlcrrj/dphy}='<ღ(¯`◕‿◕´¯) ♫ ♪ ♫ 𝓦ᗴ𝐛Ⓟ𝐀gⒺ Ⓜ𝑜Đ ♫ ♪ ♫ (¯`◕‿◕´¯)ღ>'
+{$2y$04$dYJ57xPN0XW7ePVBsGlokOVBlYvuHEdn1wNhLcnFIDvoTI7Yl6OQO}='<☆♥  Ｅᑎ𝔠𝓻ⓨＰ𝕥єĐ 𝐓є𝕏丅  ☢🌷>'
+{$2y$04$G3iNqizGqO2vugrHp80pK.TC2N1m1Bs8IjkskTSGRqKTgkYC29WBq}='<🐸  🎀  𝒶𝒹𝓋𝑒𝓇𝓉𝒾𝓈𝑒𝓂𝑒𝓃𝓉  🎀  🐸>'
+{$2y$04$DTjJigJZWH6zHBT1Qvq6ZesqcaQbDz74WkpBYdnYJ14780kImrYVi}='<🍑 ⋆ 🍇  🎀  𝒶𝒹𝓋𝑒𝓇𝓉𝒾𝓈𝑒𝓂𝑒𝓃𝓉  🎀  🍇 ⋆ 🍑>'
+{$2y$04$noPdEyl6a8qAiZ2LnbR6teUp7bFROv9/8ae9k5c5mbM3jbZ4jjU4S}='<✮✩  🎀  𝒶𝒹𝓋𝑒𝓇𝓉𝒾𝓈𝑒𝓂𝑒𝓃𝓉  🎀  ✩✮>'
+{$2y$04$qUabyosn3Dfo6z9rhrbTQOtHASWJiKhu8AOhfnv.YNyFgCjSK52W.}='<♗♛  𝒶𝒹Ｖέℝт𝔦ｓᗴⓂｅⓃŤ  ☝🎉>'
+{$2y$04$rN9hIizDKMHjjrPpHclp7ur//t2rkt8MDLN4nAyjE16EcZtutjENG} = '<♩😲  𝒶𝒹𝕍𝑒ℝ𝓣丨รε𝓶Ｅ𝔫т  🐉🐣>'
+{$2y$04$6AdSdu47RiGh5yuWZqoGQ.5WxFfT268T4cqAze4S5n1NydqrD3Cv.}='<(¯`·.¸¸.·´¯`·.¸¸.-> ⓐ𝐝ν€ᖇтĮＳ𝐞Ⓜｅ𝓝𝓽 <-.¸¸.·´¯`·.¸¸.·´¯)>'
+{$2y$04$kvcdO8wP4SCatVzyzrfvB.GqxH5HReH4R75spEn.6YrA7jL.rFRwm}='<.•°¤*(¯`★´¯)*¤° 卂∂𝕍ᵉ尺𝐓ι𝓼𝑒Ⓜ𝐄𝕟Ť °¤*(¯´★`¯)*¤°•.>'
+{$2y$04$aUnoOOdMiMT0KpAruk5wz.0539QSBsX627sP6ZRkY5P0rKwcqYckq}='<ıllıllı 卂đⓥ𝕖Ⓡ𝓣𝕀ᔕｅ𝓶𝔢ⓃⓉ ıllıllı>'
+{$2y$04$xJiYNNPRV17qNkKQShKZMOwsN/IqudU7.C0gUzvBZ5al0yS.fOOey}='<👽🐻  ＴⒺ𝓍𝐓 - 𝓜𝐨𝕕  🎄💘>'
+{$2y$04$7upPAWEt214xPJnZZpFFgO/jBq5odB.AMewJMw73xPG7n8U3qhaRi}:'<$2y$04$xK/2pQb0EhQjxWM77H/mk.Yfn0hnCZdtGMFGgEozuEqZovgzBR5jq>'
+{$2y$04$zRlyNwBmmFQSqQwRffWg2O7knohu9zDZLcNkoP/X6lCAdVxdThN9W}":'<$2y$04$dmX20ZnSuTDBtFatlCDBR.nIeLBigfYgoJTsdajwKj4fTZADSGDV.>'
+{$2y$04$MgREMtIjiaFcaf8PVq3cFeWRSb8Y2pKsjUhoBcWHiSen8tV.iJbfG}" :'<https://www.localhost:8080.go/proton>'
+> cont...
+localhost:80
+{
+reverse_proxy /db/ phpmyadmin:8080 
+reverse_proxy *goserver:3000/
+}
+..\net-xkey-scan_
+["(https://github.com/TheProdigyLeague(c)/vulnScan/work-flows/CLI/badge.svg?branch=master)"]:
+('https://github.com/TheProdigyLeague(c)/vulnScan/action/work-flows/CLI.yml')
+['Apache_License']:$
+[('https://img.shields.io/badge/License-Apache%20v2-green.svg')]
+('https://github.com/TheProdigyLeague(c)/vulnScan/blob/master/LICENSE')
+[!]:$
+['Twitter']:$
+[("https://img.shields.io/badge/Twitter-@iotscan-blue.svg")]:$
+["GitHub_Gang"]$
+('https://img.shields.io/github/gang/crime/mob')
+["repo_size"]
+["('https://img.shields.io/github/repo-size/OWASP/Nettacker)"]: (https://github.com/OWASP/Nettacker)
+<img src="https://raw.githubusercontent.com/zdresearch/OWASP-Nettacker/master/web/static/img/owasp-nettacker.png" width="200">
+<img src="https://raw.githubusercontent.com/zdresearch/OWASP-Nettacker/master/web/static/img/owasp.png" width="500">
+
+                                                                 FED GOVT$
+
+*帮派成员用于犯罪的软件。...*$
+[!]: [2018-01-19_0-45-07]:: (https://user-images.githubusercontent.com/7676267/35123376-283d5a3e-fcb7-11e7-9b1c-92b78ed4fecc.gif)$
+$2y$05$6DD5RFdGT2qcG.lJjoIEnuNjyFEvnffosNm8QeREGUneRm5O6V/e2
+$VULN_SCAN&&GENERATES REPORT FOR CIVILIAN NETWORK TRAFFICKING...SRVC_BUG_VULN_MIS.config-.-info$...
+
+                             This software **will** utilize TCP SYN, ACK, ICMP, and many other protocols in order to detect and bypass Firewall/IDS/IPS devices. $
+By leveraging a unique method for discover...'protected'.srvc&&%m.device, such as SCADA. $(c)(tm)anti-competition notice...$
+`$____________$`
+*instalar turbo!$
+*M-fM-8M-&M-hM-<M-*M-eM-.M-^IM-hM-#M-^]M-oM-<M-^A$
+*M-cM-^BM-?M-cM-^CM-<M-cM-^CM-^\M-hM-#M-^EM-gM-^]M-^@M-oM-<M-^A$
+*M-`M-$M-^_M-`M-$M-0M-`M-%M-^MM-`M-$M-,M-`M-%M-^K M-`M-$M-8M-`M-%M-^MM-`M-$M-%M-`M-$M->M-`M-$M-*M-`M-$M-?M-`M-$M-$!$
+                                                                                                                                                    M-XM-*M-XM-+M-XM-(M-YM-^JM-XM-* M-XM-*M-YM-^HM-XM-1M-XM-(M-YM-^H!*$
+============$
+/**local db default result path**/
+                                                                                                                                                                                                             `.data/nettacker.db`
+                                                                                                                                                                                                             `.dat/results` 
+ // eof
