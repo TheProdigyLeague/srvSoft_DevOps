ca4a4609c90e20369b1ec3f692a3184708695ad7
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-07-09 20:41:39 -0500

    ^d

---
+++ bash/v.bash
@@ -0,0 +1,20 @@
+$ argparse==0.0.1
+$ netaddr==0.0.1
+$ ipaddr==0.0.1
+$ requests==0.0.1
+$ paramiko==0.0.1
+$ texttable==0.0.1
+$ PySocks==0.0.1 
+library_name=socks
+mod_name
+$ pyOpenSSL==0.0.1 
+library_name=OpenSSL
+$ flask==0.0.1
+$ SQLAlchemy>=0.0.1
+library_name=sqlalchemy
+$ py3DNS==0.0.1
+library_name=DNS
+$ numpy==0.0.1
+$ terminable_thread==0.0.1
+$ PyYAML==6.0 
+library_name=yaml
