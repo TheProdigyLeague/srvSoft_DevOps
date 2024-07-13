fe9627d5c9b1162ec3a575a1609932233a2c7532
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-07-13 17:20:22 -0500

    :wrench: :file_folder:

---
+++ py/sdk/フレックスキット.kt
@@ -0,0 +1,54 @@
+# 👣😂  ⒼⒾ𝕋ʰᵘ𝓑  商標プロパティ: null アップデータ、パッケージ エコシステムの値が開発者の値と一致しません。| "dev.kt" 
+_npm
+_bundler
+_composer
+_devcontainers
+_maven
+_mix
+_cargo
+_gradle
+_nuget
+_gomod
+_docker
+_elm
+_gitsubmodule
+_github-actions
+_pip
+_terraform
+_pub
+_swift
+
+#/update/0/github-actions
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30621.155
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "TrapWire", "TrapWire\TrapWire.csproj", "{99EC22BB-FFC5-44E3-BCA2-CE6CE5C105E4}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{99EC22BB-FFC5-44E3-BCA2-CE6CE5C105E4}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{99EC22BB-FFC5-44E3-BCA2-CE6CE5C105E4}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{99EC22BB-FFC5-44E3-BCA2-CE6CE5C105E4}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{99EC22BB-FFC5-44E3-BCA2-CE6CE5C105E4}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {D61D3714-3626-41F9-BF98-DFE1151A334E}
+	EndGlobalSection
+EndGlobal
+
+<Project Sdk="Microsoft.NET.Sdk.Web">
+
+  <PropertyGroup>
+    <TargetFramework>netcoreapp3.1</TargetFramework>
+  </PropertyGroup>
+
+</Project>
