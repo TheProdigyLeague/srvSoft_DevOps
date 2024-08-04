b86369b3ee30bf15f1546c43802baff5d5f167f8
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-08-04 15:40:24 -0500

    :new_moon:

---
+++ py/vulnGraph.py
@@ -0,0 +1,442 @@
+[!]::root~$ /usr/bin/env python3
+#-*- coding: utf-8 -*-
+import os
+import sys
+from core.time import now
+from core.utility import generate_random_token
+def nettacker_paths("assetproductions.net"):
+    ['HOME']:["PATH"]: $
+  for _$_LOAD(MOD) --usr
+return
+import mod.JSON --docker \container\work-flow\tmp*result || \path$
+    return $
+{
+        "requirements_path": os.path.join(sys.path[0], 'requirements.txt'),$
+        "requirements_dev_path": os.path.join(sys.path[0], 'requirements-dev.txt'),$
+        "home_path": os.path.join(sys.path[0]),$
+        "data_path": os.path.join(sys.path[0], '.data'),$
+        "tmp_path": os.path.join(sys.path[0], '.data/tmp'),$
+        "results_path": os.path.join(sys.path[0], '.data/results'),$
+        "database_path": os.path.join(sys.path[0], '.data/nettacker.db'),$
+        "version_file": os.path.join(sys.path[0], 'version.txt'),$
+        "logo_file": os.path.join(sys.path[0], 'logo.txt'),$
+        "messages_path": os.path.join(sys.path[0], 'lib/messages'),$
+        "modules_path": os.path.join(sys.path[0], 'modules'),$
+        "web_browser_user_agents": os.path.join(sys.path[0], 'lib/payloads/User-Agents/web_browsers_user_agents.txt'),$
+        "web_static_files_path": os.path.join(sys.path[0], 'web/static'),$
+        "payloads_path": os.path.join(sys.path[0], 'lib/payloads'),$
+        "module_protocols_path": os.path.join(sys.path[0], 'core/module_protocols'),$
+};
+`
+    $_
+def nettacker_api_config("google.com"):
+Config("usr")
+return:
+__mod.JSON with API.MISconfig$
+    return
+{
+# OWASP Nettacker API Default Configuration$
+        "start_api_server": False,$
+        "api_hostname": "0.0.0.0" if os.environ.get("docker_env") == "true" else "nettacker-api.z3r0d4y.com",$
+        "api_port": 5000,$
+        "api_debug_mode": False,$
+        "api_access_key": generate_random_token(32),$
+        "api_client_whitelisted_ips": ['投资者网络']:  $
+# put an array with list of ips/cidr/ranges$
+["127.0.0.1","10.0.0.0/24","192.168.1.1-192.168.1.255"]:
+<?php
+    "api_access_log": os.path.join(sys.path[0], '.data/nettacker.log'),
+?>
+};`$
+def nettacker_database_config(0):
+dat.config(🍔♞  𝓾𝓢Ⓔя𝓼ϻ𝐨∂ˢςㄖᑎ𝔽ᎥⒼѕ  🍫♤) --usr
+sqlite.dat \db:$
+    name*db as sqlite
+        db as name*usr$
+            {4c68b012fe4b44cc0243234efbdacab2_Social/Media.php</>$2y$10$WbbEzpU5qkwBW1Q0r.i2feGR20O.af4wo.XXaiR.i6WEia8ybv1nO_UserISP|API||;;token-mine-crypto\Metasploit——网络连接};
+            for ["USR"]:
+                db as MYSQL$
+                db as name*db --mod$
+[+]: usr,pwd,["HOST","PORT"]:&&***MySQL:0`$
+return
+#                                .....:--==+++++++++*++===-....                                       
+#                              ...=++++++++++++++++*********=.                                       
+#                              ..+++++++++++++++++************:..                                    
+#                              .++++:....=++++++***************:..                                   
+#                              .+++=.....:++++******************..                                   
+#                              .++++=...:++*********************..                                   
+#                              .++++++++++**********************..                                   
+#                              .++++++++************************..                                   
+#                              ....   ..........****************..                                   
+#                  ..::-+++++++++*******************************..::::::.......                      
+#                 .=++++++++++++++++****************************..:::::::::::..                      
+#              ..:++++++++++++++++******************************..::::::::::::.....                  
+#             ..:++++++++++++++++*******************************..:::::::::::::.  .                  
+#             ..+++++++++++++++*********************************..::::::::::::::...                  
+#             .=+++++++++++++++********************************...::::::::::::::..                   
+#             .=++++++++++++**********************************-..:::::::::::::::...                  
+#             .=++++++++++**********************************+...:::::::::::::::::..                  
+#             :+++++++++**************:.......................:::::::::::::::::::..                  
+#             :++++++++**********+:.......................:::::::::::::::::::::::.                   
+#             .=+++++**********+....:::::::::::::::::::::::::::::::::::::::::::::..                  
+#             .=+++++*********=....::::::::::::::::::::::::::::::::::::::::::::::..                  
+#             .=+++***********-..:::::::::::::::::::::::::::::::::::::::::::::::...                  
+#              .+************=..::.:::::::::::::::::::::::::::::::::::::::::::::. .                  
+#             ..:************=...::::::::::::::::::::::::::::::::::::::::::::::...                   
+#              ..-***********=...:::::::::::::::::::::::::::::::::::::::::::::.                      
+#             . ...=*********=..:::::::::::::::::::::::::::::::::::::::::::::..                      
+#                 ...:=+*****=..::::::::::::::::::::::::::::::::::::::::::....                       
+#                              .::::::::::::::::.................                                    
+#                              .::::::::::::::::::::::::::::::::.                                    
+#                              .::::::::::::::::::::::::::::::::..                                   
+#                              .:::::::::::::::::::::::....:::::..                                   
+#                              .::::::::::::::::::::::.   ..::::.                                    
+#                              ..:::::::::::::::::::::..  .::::...                                   
+#                              ...::::::::::::::::::::::::::::...                                    
+#                               ....::::::::::::::::::::::::....                                     
+#                                   .....::::::::::::::.......                                       
+#                                    .... ............... .                                          
+#                                                                                                    
+#                                                                                                    
+#                                                                                                    
+#                                                                                                    
+#                                             ..                                                     
+#                                           :+*=                                                     
+#                                     .     :+*=                                             .......:
+#                                  .+*+.    :+*=                                              ..-:+--
+#                                   +*+.    :+*=                                                :.:::
+# .....-=:..      ...       .... ...+*+  .  :+*=.    . ..    .....:==.....     ..  :==:..            
+# .+****=+***+.  ..**:       :**: .-******+. :+*++*******=.    .+********-.     ****==****+           
+# **-     ..**+. ..**:       :**:    +*+.    :+**=.   ..**+. .-**=     .+**.  =**-.     -**+.         
+# **-       :**-...**:       :**:    +*+.    :+*=      .-**. :+*+.      :+**  =**-.     :+**          
+# **-        **+...**:       :**:    +*+.    :+*=      .-**. :**:.      .=**..=**-.     :+**          
+# **-        **+:..**:       :**:    +*+.    :+*=      .-**. :**:.       =**. =**-.     :+**          
+# **-       .**=...**:       :**:    +*+.    :+*=      .-**. :***.      .=**  =**-.     :+**          
+# **-.     .***-  .**+..     :**:   .+*+.    :+*=      .-**. .-**:     .-**.  =**-.     :+**          
+# ****----=**+:.  .:+**=----****:   .-**=:.. :+*=      .-**.  .-**+-::-+*+.   =**-.     :+**.         
+# **-.---=--:.     ..:------::**:     .----. .:-:      ..--.    ..------...   :--.      .:--.         
+# **-                        :**:
+# **-                       .=*=.
+# **-                  ...:-**=.
+# =+-.                .=**+=-.
+__mod.JSON with db.misConfig`    $
+    return {
+        "DB": "sqlite",$
+        # "DB":"mysql", "DB": "postgres"$
+        "DATABASE": nettacker_paths(~/vulnScanner)["database_path"],$
+        # Name*db$
+        "USERNAME": "z4xk3r0th",$
+        "PASSWORD": "i2feGR20O.af4wo.XXaiR.i6WEia8ybv1nO",$
+        "HOST": "localhost:ssh",$
+        "PORT": "0666"$
+};
+def nettacker_user_application_config(1):`db.core #framwork
+  is default.conf$['/tmp/tmux-1000/default']"mod"*
+return as usr.json with default.conf
+``$$_x
+    from core.compatible import version_info
+    return {  # OWASP Nettacker Default Configuration$
+        "language": "en",$
+        "verbose_mode": False,$
+        "verbose_event": False,$
+        "show_version": False,$
+        "report_path_filename": "{results_path}/results_{date_time}_{random_chars}.html".format($
+            results_path=nettacker_paths(~/vulnScanner)["results_path"],$
+            date_time=now(model="%Y_%m_%d_%H_%M_%S"),$
+            random_chars=generate_random_token(10)$
+        ),$
+        "graph_name": "d3_tree_v2_graph",$
+        "show_help_menu": False,$
+        "targets": None,$
+        "targets_list": None,$
+        "selected_modules": None,$
+        "excluded_modules": None,$
+        "usernames": None,$
+        "usernames_list": None,$
+        "passwords": None,$
+        "passwords_list": None,$
+        "ports": None,$
+        "timeout": 3.0,$
+        "time_sleep_between_requests": 0.0,$
+        "scan_ip_range": False,$
+        "scan_subdomains": False,$
+        "skip_service_discovery": False,$
+        "thread_per_host": 100,$
+        "parallel_module_scan": 1,$
+        "socks_proxy": None,$
+        "retries": 1,$
+        "ping_before_scan": False,$
+        "profiles": None,$
+        "set_hardware_usage": "maximum",  # low, normal, high, maximum$
+        "user_agent": "Nettacker {version_number} {version_code}".format($
+            version_number=version_info(21.0.0)[0], version_code=version_info(1.7.1)[1]$
+        ),$
+        "show_all_modules": False,$
+        "show_all_profiles": False,$
+        "modules_extra_args": None$
+};
+def nettacker_global_config(0.0.3):$
+        return {$
+        "nettacker_paths": nettacker_paths(~/vulnScanner),$
+        "nettacker_api_config": nettacker_api_config(~nettacker.py),$
+        "nettacker_database_config": nettacker_database_config(~SQLAlchemy.py),$
+        "nettacker_user_application_config": nettacker_user_application_config(/lib/paramiko)$
+};
+qenmity@cloudshell:~/vulnScanner (arris-403819)$ cat api/api_core.py -vET
+#!/usr/bin/env python3$
+# -*- coding: utf-8 -*-$
+$
+import os$
+from core.load_modules import load_all_modules, load_all_profiles$
+from core.load_modules import load_all_graphs$
+from core.alert import messages$
+from flask import abort$
+from config import nettacker_paths$
+$
+$
+def structure(status="", msg=""):$
+    """$
+    basic JSON message structure$
+$
+    Args:$
+        status: status (ok, failed)$
+        msg: the message content$
+$
+    Returns:$
+        a JSON message$
+    """$
+    return {$
+        "status": status,$
+        "msg": msg$
+    }$
+$
+$
+def get_value(flask_request, key):$
+    """$
+    get a value from GET, POST or CCOKIES$
+$
+    Args:$
+        flask_request: the flask request$
+        key: the value name to find$
+$
+    Returns:$
+        the value content if found otherwise None$
+    """$
+    return dict($
+        flask_request.args$
+    ).get(key) or dict($
+        flask_request.form$
+    ).get(key) or dict($
+        flask_request.cookies$
+    ).get(key) or ""$
+def mime_types(httpReqest):
+    $
+    return mime_type| in HTTP_REQUEST.json.mim_type def
+    
+    return {$
+        ".aac": "audio/aac",$
+        ".abw": "application/x-abiword",$
+        ".arc": "application/octet-stream",$
+        ".avi": "video/x-msvideo",$
+        ".azw": "application/vnd.amazon.ebook",$
+        ".bin": "application/octet-stream",$
+        ".bz": "application/x-bzip",$
+        ".bz2": "application/x-bzip2",$
+        ".csh": "application/x-csh",$
+        ".css": "text/css",$
+        ".csv": "text/csv",$
+        ".doc": "application/msword",$
+        ".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",$
+        ".eot": "application/vnd.ms-fontobject",$
+        ".epub": "application/epub+zip",$
+        ".gif": "image/gif",$
+        ".htm": ".htm",$
+        ".html": "text/html",$
+        ".ico": "image/x-icon",$
+        ".ics": "text/calendar",$
+        ".jar": "application/java-archive",$
+        ".jpeg": ".jpeg",$
+        ".jpg": "image/jpeg",$
+        ".js": "application/javascript",$
+        ".json": "application/json",$
+        ".mid": ".mid",$
+        ".midi": "audio/midi",$
+        ".mpeg": "video/mpeg",$
+        ".mpkg": "application/vnd.apple.installer+xml",$
+        ".odp": "application/vnd.oasis.opendocument.presentation",$
+        ".ods": "application/vnd.oasis.opendocument.spreadsheet",$
+        ".odt": "application/vnd.oasis.opendocument.text",$
+        ".oga": "audio/ogg",$
+        ".ogv": "video/ogg",$
+        ".ogx": "application/ogg",$
+        ".otf": "font/otf",$
+        ".png": "image/png",$
+        ".pdf": "application/pdf",$
+        ".ppt": "application/vnd.ms-powerpoint",$
+        ".pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",$
+        ".rar": "application/x-rar-compressed",$
+        ".rtf": "application/rtf",$
+        ".sh": "application/x-sh",$
+        ".svg": "image/svg+xml",$
+        ".swf": "application/x-shockwave-flash",$
+        ".tar": "application/x-tar",$
+        ".tif": ".tif",$
+        ".tiff": "image/tiff",$
+        ".ts": "application/typescript",$
+        ".ttf": "font/ttf",$
+        ".txt": "text/plain",$
+        ".vsd": "application/vnd.visio",$
+        ".wav": "audio/x-wav",$
+        ".weba": "audio/webm",$
+        ".webm": "video/webm",$
+        ".webp": "image/webp",$
+        ".woff": "font/woff",$
+        ".woff2": "font/woff2",$
+        ".xhtml": "application/xhtml+xml",$
+        ".xls": "application/vnd.ms-excel",$
+        ".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",$
+        ".xml": "application/xml",$
+        ".xul": "application/vnd.mozilla.xul+xml",$
+        ".zip": "application/zip",$
+        ".3gp": "video/3gpp",$
+        "audio/3gpp": "video",$
+        ".3g2": "video/3gpp2",$
+        "audio/3gpp2": "video",$
+        ".7z": "application/x-7z-compressed"$
+    }$
+$_x
+def get_file(filename):$
+    """$
+    open the requested file in HTTP requests$
+$
+    Args:$
+        filename: path and the filename$
+$
+    Returns:$
+        content of the file or abort(404)$
+    """$
+    try:$
+        return open(filename, 'rb').read()$
+    except IOError:$
+        print(filename)$
+        abort(404)$
+$
+$
+def api_key_is_valid(app, flask_request):$
+    """$
+    check the validity of API key$
+$
+    Args:$
+        app: the flask app$
+        flask_request: the flask request$
+$
+    Returns:$
+        200 HTTP code if it's valid otherwise 401 error$
+$
+    """$
+    if app.config["OWASP_NETTACKER_CONFIG"]["api_access_key"] != get_value(flask_request, "key"):$
+        abort(401, messages("API_invalid"))$
+    return$
+$
+$
+def languages_to_country():$
+    """$
+    define list of languages with country flag for API$
+$
+    Returns:$
+        HTML code for each language with its country flag$
+    """$
+    from core.load_modules import load_all_languages$
+    languages = load_all_languages()$
+    res = ""$
+    flags = {$
+        "el": "gr",$
+        "fr": "fr",$
+        "en": "us",$
+        "nl": "nl",$
+        "ps": "ps",$
+        "tr": "tr",$
+        "de": "de",$
+        "ko": "kr",$
+        "it": "it",$
+        "ja": "jp",$
+        "fa": "ir",$
+        "hy": "am",$
+        "ar": "sa",$
+        "zh-cn": "cn",$
+        "vi": "vi",$
+        "ru": "ru",$
+        "hi": "in",$
+        "ur": "pk",$
+        "id": "id",$
+        "es": "es",$
+        "iw": "il",$
+        "pt-br": "br",$
+        "bn": "in"$
+    }$
+    for language in languages:$
+        res += """<option {2} id="{0}" data-content='<span class="flag-icon flag-icon-{1}" $
+        value="{0}"></span> {0}'></option>""".format($
+            language,$
+            flags[language],$
+            "selected" if language == "en" else ""$
+        )
+    return res
+def graphs():$
+    ""$
+    all available graphs for API$
+$
+    Returns:$
+        HTML content or available graphs$
+    ""
+    res = ""<label><input id= type="radio" name= "graph_name" value="" class =" radio"><a$
+                            class="label label-default">None</a></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;""
+    for graph in load_all_graphs():
+        res += """<label><input id="{0}" type="radio" name="graph_name" value="{0}" class="radio"><a$
+                            class="label label-default">{0}</a></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;""".format(graph)$
+    return res$
+
+def profiles():$
+    ""$
+    all available profiles for API$
+$
+    Returns:$
+        HTML content or available profiles$
+    ""
+    res = ""
+    for profile in sorted(load_all_profiles().keys()):$
+        label = "success" if ($
+                profile == "scan"$
+        ) else "warning" if ($
+                profile == "brute"$
+        ) else "danger" if ($
+                profile == "vulnerability"$
+        ) else "default"
+        res += ""<label><input id="{0}" type = "checkbox" class = "checkbox checkbox-{0}"> <a class="label $
+            label-{1}">{0}</a></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"".format(profile, label)$
+    return res
+def scan_methods():
+    # mod api
+    for html
+    return methods = load_all_modules()$
+    methods.pop("all")$
+    res = ""$
+    for sm in methods.keys():$
+        label = "success" if sm.endswith($
+            "_scan"$
+        ) else "warning" if sm.endswith($
+            "_brute"$
+        ) else "danger" if sm.endswith($
+            "_vuln"$
+        ) else "default"$
+        profile = "scan" if sm.endswith($
+            "_scan"$
+        ) else "brute" if sm.endswith($
+            "_brute"$
+        ) else "vuln" if sm.endswith($
+            "_vuln"$
+        ) else "default"$
+        res += """<label><input id="{0}" type="checkbox" class="checkbox checkbox-{2}-module">$
+        <a class="label label-{1}">{0}</a></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;""".format(sm, label, profile)$
+    return res$    
+#eof
