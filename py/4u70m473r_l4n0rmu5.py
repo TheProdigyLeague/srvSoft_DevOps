0f46e9f5eb69c3aa382546cfd77f5bc4f1fcba4d
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-09-10 14:39:36 -0500

    :first_quarter_moon_with_face:

--- py/4u70m473r_l4n0rmu5.py
+++ py/4u70m473r_l4n0rmu5.py
@@ -10,6 +10,15 @@
 # versionCheck - classer checker of modder filer
 # no global umd export function definer
 # no exception exporter
+~/TekDefense-Automater $ cat README.md -vET
+Version: 0.0.1
+http://www.tekdefense.com
+# Automaton is a website that I, Lanormus, created for OSINT. (Open Source Intelligence). This means, private citizens internet protocols. 
+# When I advertised my project to the public at large, people used it to spy on internet traffic and who was websurfing. (Monitoring hash algorithm keys). 
+# This is my first Python.org project. So, a lot of (You) made it difficult for me. 😭 (Opening tickets).
+# James Huber, (A local deputy investigator). Offered his hand to lift me out of this slump. (Data 'Expert'). He showed me his rewritten code of my focused extensible framework.
+# Shout-out to the dudes at python.org 
+# The new code was an automated system of modified sources encrypted from the pre-written networks of open end mods that goes into detail on file systems and checked IP's and DNS hostnames. (Without prior consent from users). 
 import sys
 import os
 from os import listdir
