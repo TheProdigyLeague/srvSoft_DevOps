0d2a88c33d45d56255e7d9fd09744eac1e4c17ac
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-09-27 12:55:56 -0500

    job markets
    ⏳

---
+++ sql/cloud/tokens.py
@@ -0,0 +1,255 @@
+13.110.78.205
+import godMode
+export cloudFare => https://d3216uwaav9lg7.cloudfront.net
+#aws
+<metaname="csp-nonce" content="gY4di8cVpVJOnQPi89JkuQ==">
+[ thread::yCJP03YZfsqWIllrfSnlmC4:: ]
+import data-faq="https://prod.contactus.jobsatamazon.hvh.a2z.com/united-states/us-cf-past-employees-en"
+import data-position="客服代表" 
+import data-position="カスタマーサービス"
+import data-position="Virtueller Kundenservice"
+import data-position="Warehouse / Fulfillment / Delivery"
+import data-position="المستودع / الشحن / مندوب توصي"
+import data-country="India"
+*cloudfront.net
+~ </meta>
+  break
+  export AES_128_GCM
+
+
+  <
+meta data-hvh-position="contact-us-working-hour-config">
+
+data-banner-text=
+"Please excuse the delay! break Our team is handling a large number of queries from candidates at the moment. This means our response to you may be delayed 7-10 business days.  We appreciate your patience.  **For queries about Warehouse roles, please avoid selecting the Customer Service option as you will not be directed to the right team.  Refer to the Common Questions on this page for general questions, and use the individual country drop-down on this page to find Common Questions and contact options for Warehouse positions.   If your query is about an employment offer, please respond to your offer email for assistance.**"
+>
+
+<linkhref = "https://hiring.amazon.com/app/main.prod.css" rel = "stylesheet" nonce = "gY4di8cVpVJOnQPi89JkuQ==">
+  <style nonce="gY4di8cVpVJOnQPi89JkuQ==">
+    html {
+      scroll-padding-top: 135px;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+
+  
+  <script nonce="gY4di8cVpVJOnQPi89JkuQ==">
+    export function getNonce() {
+      return "gY4di8cVpVJOnQPi89JkuQ==";
+    }
+
+    from __webpack_nonce__ = getNonce();
+    from window.__webpack_nonce__ = getNonce();
+  </script>
+  
+break
+
+from "https://d3216uwaav9lg7.cloudfront.net/assets-HVHCareer.css" | "https://d3216uwaav9lg7.cloudfront.net/assets-HVHCareer.js"
+
+<script nonce="gY4di8cVpVJOnQPi89JkuQ==">
+  (function () {
+    function getCookie(name) {
+      const cookies = '; ' + document.cookie;
+      const separator = "; " + name + "=";
+      const parts = cookies.split(separator);
+
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    import function addAdobeAndResizerScript() {
+      from const iframe_resizer = document.createElement("script");
+      iframe_resizer.type = "text/javascript";
+      iframe_resizer.src = "/resources/js/iframe_resizer.js";
+      iframe_resizer.nonce = "gY4di8cVpVJOnQPi89JkuQ==";
+
+      export document.head.appendChild(iframe_resizer);
+
+      if ("false" === "false" || getCookie("hvh-analytics-cookies-enable") !== 'false') {
+        import const adobeScript = document.createElement("script");
+        adobeScript.type = "text/javascript"
+        adobeScript.src = "https://assets.adobedtm.com/launch-EN6bc43e7eee5146beaa43d868366aabe6.min.js";
+        adobeScript.nonce = "gY4di8cVpVJOnQPi89JkuQ==";
+
+       export document.body.appendChild(adobeScript);
+
+        const adobe_analytics = document.createElement("script");
+        adobe_analytics.type = "text/javascript";
+        adobe_analytics.src = "/resources/js/adobe_analytics.js";
+        adobe_analytics.nonce = "gY4di8cVpVJOnQPi89JkuQ==";
+
+       export document.body.appendChild(adobe_analytics);
+      }
+    }
+
+    if (document.readyState === "complete") {
+      addAdobeAndResizerScript();
+    } else {
+      window.addEventListener('load', addAdobeAndResizerScript);
+    }
+  })()
+</script>
+> https://www.youtube.com/embed/SHapxk9c45Q?rel=0&showinfo=0&controls=0&iv_load_policy=3&autoplay=1&mute=1&cc_load_policy=0&enablejsapi=1&origin=https%3A%2F%2Fcareers.walmart.com&widgetid=1:formatted
+<8GH3a000000000000000000000000000000000000000000000SKH9C300Wk7gdsUQRs20YFWJ0teOeg@sfdc.net>
+<applicationhelp@amazon.com>
+break
+<script>try{Typekit.load({async: true});}catch(e){}</script>
+"https://walmart-careers.scdn3.secure.raxcdn.com/resource/css/main.min.790821d5dab5fe9824980c1a2c6c9710.gz.css"
+from https://support.google.com/a?p=show_original&hl=en
+from https://assets.adobedtm.com/5495d9872198d574fae7791c47c0d2e1c24ef51d/s-code-contents-87886e55f5483d7e9ac1119289947216214f84d0.js:formatted
+from https://connect.facebook.net/en_US/fbevents.js:formatted # META PUPPETEERING
+except: FBEventsCoercionError  
+a (https://connect.facebook.net/en_US/fbevents.js:222:555) 
+https://connect.facebook.net/en_US/fbevents.js:222:805 
+A (https://connect.facebook.net/en_US/fbevents.js:222:2009)
+def (https://connect.facebook.net/en_US/fbevents.js:222:2546) 
+B (https://connect.facebook.net/en_US/fbevents.js:222:2103) 
+https://connect.facebook.net/en_US/fbevents.js:222:2410 
+A (https://connect.facebook.net/en_US/fbevents.js:222:2009) 
+e (https://connect.facebook.net/en_US/fbevents.js:56:238) 
+Function.Z [as init] (https://connect.facebook.net/en_US/fbevents.js:305:7481) 
+Function.Y [as callMethod] (https://connect.facebook.net/en_US/fbevents.js:305:2519)
+name : "FBEventsCoercionError"
+message : ""
+stack: "FBEventsCoercionError\n    
+a (https://connect.facebook.net/en_US/fbevents.js:222:555)\n    
+@https://connect.facebook.net/en_US/fbevents.js:222:805\n    
+A (https://connect.facebook.net/en_US/fbevents.js:222:2009)\n    
+ def (https://connect.facebook.net/en_US/fbevents.js:222:2546)\n    
+B (https://connect.facebook.net/en_US/fbevents.js:222:2103)\n    
+https://connect.facebook.net/en_US/fbevents.js:222:2410\n  
+A (https://connect.facebook.net/en_US/fbevents.js:222:2009)\n  
+e (https://connect.facebook.net/en_US/fbevents.js:56:238)\n   
+Function.Z [as init] (https://connect.facebook.net/en_US/fbevents.js:305:7481)\n   
+Function.Y [as callMethod] (https://connect.facebook.net/en_US/fbevents.js:305:2519)"
+> [[Prototype]]: Object
+=> this: undefined
+a : "1642560762664754"
+if (!l)
+            try {
+                var k = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
+                l = k.GetVariable("$version")
+            } catch (w) {}
+break
+export XMLHttpRequest = 20%
+break
+import function W(e) {
+        var t, n = e.getAttribute("data-options");
+        try {
+            t = JSON.parse(n.replace(/'/g, '"'))
+        } catch (e) {
+            t = {}
+        }
+        return t
+    }
+ a.Kb = function() {
+        if (!a.Tb) {
+            var c = new Date, b = n.location, d, f, e = f = d = "", g = "", h = "", l = "1.2", k = a.cookieWrite("s_cc", "true", 0) ? "Y" : "N", m = "", p = "";
+            if (c.setUTCDate && (l = "1.3",
+            (0).toPrecision && (l = "1.5",
+            c = [],
+            c.forEach))) {
+                l = "1.6";
+                f = 0;
+                d = {};
+                try {
+                    f = new Iterator(d),
+                    f.next && (l = "1.7",
+                    c.reduce && (l = "1.8",
+                    l.trim && (l = "1.8.1",
+                    Date.parse && (l = "1.8.2",
+                    Object.create && (l = "1.8.5")))))
+                } catch (r) {}
+import function Kc(a, b, c) {
+        if (Lc()) {
+            var d = Object.assign({}, Jc);
+            b && (d.body = b);
+            c && (c.attributionReporting && (d.attributionReporting = c.attributionReporting),
+            c.browsingTopics && (d.browsingTopics = c.browsingTopics));
+            try {
+                var e = C.fetch(a, d);
+                e && e.catch(kb);
+                return !0
+            } catch (f) {}
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCn7RSKlBHaN5EVPbtn53f_podPlR-xHRw&callback=onMapsAPIReady"></script>    
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1642560762664754'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<script><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1642560762664754&ev=PageView&noscript=1"
+/></script>
+</script>
+a = [],
+            m = [],
+            (h.qsa = ee.test(C.querySelectorAll)) && (ce(function(e) {
+                var t;
+                s.appendChild(e).innerHTML = "<a id='" + S + "'></a><select id='" + S + "-\r\\' msallowcapture=''><option selected=''></option></select>",
+                e.querySelectorAll("[msallowcapture^='']").length && m.push("[*^$]=" + R + "*(?:''|\"\")"),
+                e.querySelectorAll("[selected]").length || m.push("\\[" + R + "*(?:value|" + L + ")"),
+                e.querySelectorAll("[id~=" + S + "-]").length || m.push("~="),
+                (t = C.createElement("input")).setAttribute("name", ""),
+                e.appendChild(t),
+                e.querySelectorAll("[name='']").length || m.push("\\[" + R + "*name" + R + "*=" + R + "*(?:''|\"\")"),
+                e.querySelectorAll(":checked").length || m.push(":checked"),
+                e.querySelectorAll("a#" + S + "+*").length || m.push(".#.+[+~]"),
+                e.querySelectorAll("\\\f"),
+                m.push("[\\r\\n\\f]")
+            })
+                                            t = s(t)
+from https://vjt-az.modernhire.com/vjt/WalmartVJT?uid=E-136411457&pid=10&tid=1001&intsys=106&aid=26206626&chk=DFOGKJQMUMRQPSLINOJMRONOPQ68
+import SyntaxError
+import JavaScript
+import Adobe
+export analytics.js:formatted
+https://vjt-az.modernhire.com/vjta/=oauth/2/WalmartVJT/Walmart/10?applid=26206626&s=2&lang=en&tid=1001&cpath=WalmartVJT/VJT&tkn=Qu2Jn_49V1V2zfbD7mtCWUpBSNlXRnFiGgowPVZr-j0pzsBEmes-XBJiAVYm7ZHN1qLgeZl3S6GoQ9OX0X6sI4ChKZIn4Ro6eRQJqCxg2gNoFdaPw-s9JOgdVsTFUqmCkVNQ84PlO4RGSLKZMW5jutuv44gVeLf8Kh1CD8D2PqZSExyxdlfTLvpHwvN5uJ-CIeNgAOmM2wiIvN0SOnz8QMW0Ys76kg35nkulZA7TUcBumSfeOhaIla9y6KHS0poi&rnd=8392/=1
+fetch("https://www.youtube.com/youtubei/v1/log_event?alt=json&key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "SAPISIDHASH 1727453811_dcdca10edb64c80d41f2267e97077b23ba59b4b5_u",
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-client-data": "CIy2yQEIprbJAQipncoBCJKhywEIh/HLAQjv8ssB",
+    "x-goog-authuser": "0",
+    "x-goog-event-time": "1727453811807",
+    "x-goog-request-time": "1727453811807",
+    "x-goog-visitor-id": "Cgt1NTd5cUdOdjYxcyiCn9u3BjIKCgJVUxIEGgAgHQ%3D%3D",
+    "x-origin": "https://www.youtube.com",
+    "x-youtube-ad-signals": "dt=1727451013740&flash=0&frm=2&u_tz=-300&u_his=3&u_h=698&u_w=1241&u_ah=698&u_aw=1241&u_cd=24&bc=31&bih=-12245933&biw=-12245933&brdim=0%2C0%2C0%2C0%2C1241%2C0%2C1241%2C698%2C1087%2C377&vis=2&wgl=true&ca_type=image&bid=ANyPxKoQnzV_uI9C-1rMlH95f2p9d89iVVqglkR8UzHNGI4JJ6vOftA6Mq-nG2uzsAW6v5_lL1y2QrWQTc4PXG1Bk2o4SiP3gQ",
+    "x-youtube-client-name": "56",
+    "x-youtube-client-version": "1.20240924.01.00",
+    "x-youtube-time-zone": "America/Lima",
+    "x-youtube-utc-offset": "-300"
+  },
+  "referrer": "https://www.youtube.com/embed/SHapxk9c45Q?rel=0&showinfo=0&controls=0&iv_load_policy=3&autoplay=1&mute=1&cc_load_policy=0&enablejsapi=1&origin=https%3A%2F%2Fcareers.walmart.com&widgetid=1",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"US\",\"clientName\":56,\"clientVersion\":\"1.20240924.01.00\",\"configInfo\":{\"appInstallData\":\"CIKf27cGEMn3rwUQ0LixBRDDxf8SEPjMsQUQjdSxBRDrmbEFELfq_hIQlP6wBRDU1rEFEPSrsAUQjNCxBRDtubEFEMX1sAUQhcOxBRDG1LEFEOCN_xIQvZmwBRCNlLEFEN3o_hIQlpWwBRDr6P4SEOPRsAUQ78exBRDW3bAFEIzNsQUQ5s-xBRCuwbEFEJrOsQUQxqSxBRDT4a8FEN6tsQUQ-bmxBRDq0LEFEKLUsQUQj8OxBRC9tq4FEIO8sQUQ8LuxBRDH5rAFEO_NsAUQlImxBRCZmLEFEJzRsQUQzN-uBRComrAFEOrDrwUQqtiwBRCQzLEFEMHNsQUQhaexBRCmkrEFEPGcsAUQooGwBRD2q7AFEMfQsQUQjtCxBRCU_K8FEIiHsAUQ86KxBRCmk7EFEIHGsAUQn9OwBRCw7rAFEIHDsQUQop2xBRDZya8FELfvrwUQkq6xBRCKobEFEP_VsQUQiOOvBRDf7bAFEImnsQUQ5bmxBRC9irAFENCNsAUQndCwBRDJ5rAFEK_HsQUQh8OxBRCM1LEFEO_LsQUQksuxBRD4q7EFEMfKsQUqHENBTVNFUlVLLVpxLURJT3o5QXV3alFycGR4MEg%3D\"},\"userAgent\":\"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.107 Mobile Safari/537.36\",\"screenDensityFloat\":\"1.100000023841858\",\"connectionType\":\"CONN_CELLULAR_2G\"},\"thirdParty\":{\"embedUrl\":\"https://careers.walmart.com/\"}},\"events\":[{\"eventTimeMs\":1727453797242,\"latencyActionTicked\":{\"tickName\":\"pot_xe\",\"clientActionNonce\":\"f0pe8W1SyXuKqzdp\"},\"context\":{\"lastActivityMs\":\"2761568\"}},{\"eventTimeMs\":1727453811791,\"finalPayload\":{\"csn\":\"c0BWMTUXzhFBjqFw\"},\"context\":{\"lastActivityMs\":\"2776118\"}}],\"serializedClientEventId\":{\"serializedEventId\":\"gs_2Zt66DfyG_tcP8aK4cA\",\"clientCounter\":\"21614\"}}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+
+*.modernhire.com
+
+break
+
+*.startupcommon.bundle.js
+	export rbuggyQSA = rbuggyQSA.length && new RegExp( rbuggyQSA.join( "|" ) );
+	export rbuggyMatches = rbuggyMatches.length && new RegExp( rbuggyMatches.join( "|" ) );
+# assessments not caching and forcing redo.
