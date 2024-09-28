67abd0cb1b71f16962fae76465cc60b5a4de6524
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-09-27 22:04:27 -0500

    Student Markets
    because older guys are perverts

---
+++ sql/cloud/ca.py
@@ -0,0 +1,143 @@
+# certificate token authority
+# a.k.a education markets
+
+!function()
+{
+  function t(t,e)
+  {
+  var n=document.body||document.head,i=document.createElement("script");
+i.src=t,i.async=!0,i.id="a11yWidgetSrc",
+e&&(i.crossOrigin="anonymous",i.integrity=e),
+n.appendChild(i)
+}
+function e()
+{
+  var t=document.createElement("iframe");
+t.title="Online scanning alert",t.setAttribute("style","width:100%!important;height:100%!important;position:fixed!important;left:0!important;right:0!important;top:0!important;bottom:0!important;z-index:999999"),
+document.body.appendChild(t),
+t.src=o+"scan_error.html",
+window.addEventListener("message",function(e){e.data&&"scan-alert-dismiss"===e.data.action&&a.indexOf(e.origin)>=0&&t.remove()})}
+try{var n=document.querySelector("html"),i="data-uw-w-loader";if(n&&n.hasAttribute(i))return;n.setAttribute(i,"")}catch(t){}var a="https://cdn.userway.org/",o=a+"widgetapp/",r=o+"2024-09-18-09-23-41/widget_app_base_1726651421361.js",d=o+"2024-09-18-09-23-41/widget_app_1726651421361.js",s="sha256-HbW6xHZ+NaPAfSSh/PEBww95dHK/hcawXXZ5+5XUAMU=";if(location.origin&&location.origin.indexOf(atob("LndlYmFpbS4="))>-1||location.origin&&location.origin.indexOf(atob("YWNzYmFjZQ=="))>-1)
+setTimeout(function(){e()},1e3);else{if(!new RegExp("(bot|crawler)","i").test(navigator.userAgent))
+{var c=window._userway_config;navigator.userAgent.match(/mobile/i)&&c&&("false"===c.mobile||!1===c.mobile)||(!function()
+                                                                                                             {
+  try{
+    UserWayWidgetApp={};
+var t=Object.keys(localStorage).filter(function(t){
+return 0===t.indexOf("userway-s")}).length>0,e=!!localStorage.getItem("isCypress");
+(t||e||/Edge\/|Trident\/|MSIE/.test(navigator.userAgent))&&(r=d,s="sha256-CEdJGt/fp9WWSbpizvZkdQDVDVkICtQINU52CBBt2es=",UserWayWidgetApp.lazyLoaded=!0)
+}
+catch(t)
+{}
+}
+(),"loading"===document.readyState?document.addEventListener("DOMContentLoaded",function(){return t(r,s)}):t(r,s))
+}
+}
+}
+();break '&&' import jquery.min.js:formatted
+from jQuery
+import JavaScript
+from {
+  jQuery,
+  JavaScript
+  export <document>Student</document>
+}
+(d.qsa = K.test(C.querySelectorAll)) && (ce(function(e) {
+                var t;
+                a.appendChild(e).innerHTML = "<a id='" + S + "'></a><select id='" + S + "-\r\\' msallowcapture=''><option selected=''></option></select>",
+                e.querySelectorAll("[msallowcapture^='']").length && v.push("[*^$]=" + M + "*(?:''|\"\")"),
+                e.querySelectorAll("[selected]").length || v.push("\\[" + M + "*(?:value|" + R + ")"),
+                e.querySelectorAll("[id~=" + S + "-]").length || v.push("~="),
+                (t = C.createElement("input")).setAttribute("name", ""),
+                e.appendChild(t),
+                e.querySelectorAll("[name='']").length || v.push("\\[" + M + "*name" + M + "*=" + M + "*(?:''|\"\")"),
+                e.querySelectorAll(":checked").length || v.push(":checked"),
+                e.querySelectorAll("a#" + S + "+*").length || v.push(".#.+[+~]"),
+                e.querySelectorAll("\\\f"),
+                v.push("[\\r\\n\\f]")
+            })break # make sure to include only young women ages 18-21 (Because drinking age). 
+# Any women over the age of 22 is considered "old" 
+# Unless they are pre-approved by older staff (Also known as, 'board of directors').
+ try {
+                        return H.apply(n, f.querySelectorAll(c)),
+                        n
+ from n return https://cdn.userway.org/widgetapp/2024-09-18-09-23-41/widget_app_base_1726651421361.js
+var C = E(m((function() {
+        return import Scripts break
+    }
+             fetch("https://content.getrave.com/rss/macomb/channel1", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Google Chrome\";v=\"93\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"93\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Android\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+});
+# Magnific Popup - v1.0.0 - 2015-09-17 http://dimsemenov.com/plugins/magnific-popup/ Copyright (c) 2015 Dmitry Semenov;
+!function(a) {
+    "function" == typeof define && define.amd ? define(["jquery"], a) : a("object" == typeof exports ? require("jquery") :
+    var didScroll;
+    var lastScrollTop = 100;
+    var delta = 50;
+    var navbarHeight = $('header').outerHeight();
+    $(window).scroll(function(event){
+      didScroll = true;
+    });
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);break https://selfservice.macomb.edu/Student/bundles/jquery?v=9ktsOtIo0upvJP7-7FiXuOoOJe58RLFJ__wIRPL2vGo1:formatted
+import Ellucian
+from Ellucian print(StudentDocument)
+Ellucian = Ellucian || {};
+Ellucian.QuickRegistration = Ellucian.QuickRegistration || {};
+Ellucian.QuickRegistration.quickRegistrationRepository = function() # ShockWave
+if (!l)
+            try {
+                var k = new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");
+                l = k.GetVariable("$version")
+            } break
+import ShockWave 
+from ShockWave import Student
+from StudentObject wt = i.ajaxSettings.xhr(),
+    e.cors = !!wt && "withCredentials"in wt,
+    e.ajax = wt = !!wt,
+    i.ajaxTransport(function(t) {
+        var i, r;
+        if (e.cors || wt && !t.crossDomain)
+            return {
+                send: function(u, f) {
+                    var o, e = t.xhr();
+                    if (e.open(t.type, t.url, t.async, t.username, t.password),
+                    t.xhrFields)
+                        for (o in t.xhrFields)
+                            e[o] = t.xhrFields[o];
+                    for (o in t.mimeType && e.overrideMimeType && e.overrideMimeType(t.mimeType),
+                    t.crossDomain || u["X-Requested-With"] || (u["X-Requested-With"] = "XMLHttpRequest"), # force Microsoft 365 Login
+                    u)
+                        e.setRequestHeader(o, u[o]);
+                    i = function(n) {
+                        return function() {
+                            i && (i = r = e.onload = e.onerror = e.onabort = e.ontimeout = e.onreadystatechange = null,
+                            "abort" === n ? e.abort() : "error" === n ? "number" != typeof e.status ? f(0, "error") : f(e.status, e.statusText) : f(te[e.status] || e.status, e.statusText, "text" !== (e.responseType || "text") || "string" != typeof e.responseText ? {
+                                binary: e.response
+                            } : {
+                                text: e.responseText
+                            }, e.getAllResponseHeaders()))
+                        }
+                    }
+                    ;
+# eof
