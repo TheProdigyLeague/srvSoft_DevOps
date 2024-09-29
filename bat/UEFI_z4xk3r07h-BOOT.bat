b1aefbcf48a4c813749e3d690192259eba2e5a73
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-09-28 19:29:11 -0500

    September 28
    How did you convert it to digital?

---
+++ bat/UEFI_z4xk3r07h-BOOT.bat
@@ -0,0 +1,23 @@
+#MODERN DAY COMPUTER
+@echo off
+cls
+echo Loading...
+> $_M-pM-^_M-^ZM-^@ $$_M-pM-^_M-^ZM-^@ $$_M-pM-^_M-^ZM-^@ $__$_M-pM-^_M-^ZM-^@ $
+:loop
+if exist C:\ZAXKEROTH\SYSTEM86\z4xk3r07h.exe
+goto loop
+
+:start
+> C:\ZAXKEROTH\SYSTEM86\z4xk3r07h.exe 
+< z4xk3r07h_driver for "https://www.assetproductions.net/#zaxkeroth.cinnamon.brass-76yh.squarespace/"
+> z4xk3r07h buildpacks:+ -i 1 z4xk3r07h-community/assetproductions-for-testing
+> ZAXKEROTH_
+> $`ZAXKEROTH_z4xk3r07h_CHANNEL`
+< $_$_
+> C:\config\variable> Dev/deploy/build
+PATH taskset | z4xk3r07h & z4xk3r07h_driver --buildpacks rm this from deployment
+PATH assetproductions.net buildpacks:remove assetproductions.net/ZAXKEROTH-z4xk3r07h
+PATH assetproductions.net buildpacks:remove assetproductions.net/z4xk3r07h_driver
+PATH assetproductions.net buildpacks:add -i 1 assetproductions.net-community/z4xk3r07h-for-testing
+set buildpack `z4xk3r07h` & `z4xk3r07hdriver` in PATH dynos
+>>> assetproductions.net run bash
