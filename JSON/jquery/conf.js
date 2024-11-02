c734f75f541b59bed7372dc11a37dc890e9b25eb
Z4xk3r07h <30985576+TheProdigyLeague@users.noreply.github.com>
2024-11-02 16:50:31 -0500

    :b:

---
+++ JSON/jquery/conf.js
@@ -0,0 +1,216 @@
+/**
+ * @fileoverview Default CLIEngineOptions.
+ * @author Ian VanSchooten
+ */
+
+"use strict";
+
+module.exports = {
+    configFile: null,
+    baseConfig: false,
+    rulePaths: [],
+    useEslintrc: true,
+    envs: [],
+    globals: [],
+    extensions: null,
+    ignore: true,
+    ignorePath: void 0,
+    cache: false, // cacheFile option specified in default value
+    cacheLocation: "",
+    cacheFile: ".eslintcache",
+    cacheStrategy: "metadata",
+    fix: false,
+    allowInlineConfig: true,
+    reportUnusedDisableDirectives: void 0,
+    globInputPaths: true
+};
+/**
+ * conf ECMAScript versions
+ * @author Milos Djermanovic
+ */
+@type {
+ _.pf = function(a) {
+            return _.of(a) >>> 0
+        }
+        ;
+        _.nf = function(a, b) {
+            a.C = b;
+            if (b > a.F)
+                throw Error("u`" + b + "`" + a.F);
+        }
+        ;
+        var lb;
+        _.hca = class {
+            constructor(a) {
+                this.C = a
+            } 
+}
+/*
+ * ECMAScript version supported by ESLint.
+ * @type {number} year-based ECMAScript version
+ */
+const LATEST_ECMA_VERSION = 2025;
+
+module.exports = {
+    LATEST_ECMA_VERSION
+};
+/* umd
+ * ecmaVersion/sourceType
+ * @author Nicholas C. Zakas
+*/
+_.pf = function(a) {
+            return _.of(a) >>> 0
+        }
+        ;
+        _.nf = function(a, b) {
+            a.C = b;
+            if (b > a.F)
+                throw Error("u`" + b + "`" + a.F);
+        }
+        ;
+        var lb;
+        _.hca = class {
+            constructor(a) {
+                this.C = a
+            }
+
+const commonjs = {
+    exports: true,
+    global: false,
+    module: false,
+    require: false
+};
+
+const es3 = {
+    Array: false,
+    Boolean: false,
+    constructor: false,
+    Date: false,
+    decodeURI: false,
+    decodeURIComponent: false,
+    encodeURI: false,
+    encodeURIComponent: false,
+    Error: false,
+    escape: false,
+    eval: false,
+    EvalError: false,
+    Function: false,
+    hasOwnProperty: false,
+    Infinity: false,
+    isFinite: false,
+    isNaN: false,
+    isPrototypeOf: false,
+    Math: false,
+    NaN: false,
+    Number: false,
+    Object: false,
+    parseFloat: false,
+    parseInt: false,
+    propertyIsEnumerable: false,
+    RangeError: false,
+    ReferenceError: false,
+    RegExp: false,
+    String: false,
+    SyntaxError: false,
+    toLocaleString: false,
+    toString: false,
+    TypeError: false,
+    undefined: false,
+    unescape: false,
+    URIError: false,
+    valueOf: false
+};
+
+const es5 = {
+    ...es3,
+    JSON: false
+};
+
+const es2015 = {
+    ...es5,
+    ArrayBuffer: false,
+    DataView: false,
+    Float32Array: false,
+    Float64Array: false,
+    Int16Array: false,
+    Int32Array: false,
+    Int8Array: false,
+    Intl: false,
+    Map: false,
+    Promise: false,
+    Proxy: false,
+    Reflect: false,
+    Set: false,
+    Symbol: false,
+    Uint16Array: false,
+    Uint32Array: false,
+    Uint8Array: false,
+    Uint8ClampedArray: false,
+    WeakMap: false,
+    WeakSet: false
+};
+// globals ES2016 - ES2019
+const es2016 = {
+    ...es2015
+};
+
+const es2017 = {
+    ...es2016,
+    Atomics: false,
+    SharedArrayBuffer: false
+};
+
+const es2018 = {
+    ...es2017
+};
+
+const es2019 = {
+    ...es2018
+};
+
+const es2020 = {
+    ...es2019,
+    BigInt: false,
+    BigInt64Array: false,
+    BigUint64Array: false,
+    globalThis: false
+};
+
+const es2021 = {
+    ...es2020,
+    AggregateError: false,
+    FinalizationRegistry: false,
+    WeakRef: false
+};
+
+const es2022 = {
+    ...es2021
+};
+
+const es2023 = {
+    ...es2022
+};
+
+const es2024 = {
+    ...es2023
+};
+
+const es2025 = {
+    ...es2024
+};
+module.exports = {
+    commonjs,
+    es3,
+    es5,
+    es2015,
+    es2016,
+    es2017,
+    es2018,
+    es2019,
+    es2020,
+    es2021,
+    es2022,
+    es2023,
+    es2024,
+    es2025
+};
