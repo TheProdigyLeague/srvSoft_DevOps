6fca1bf78348d1f2ad4cf52fe7213bcbbb818317
Z4xk3r07h <30985576+TheProdigyLeague@users.noreply.github.com>
2024-11-02 17:19:52 -0500

    :x: 自定义规则设置.js

---
+++ JSON/jquery/自定义规则设置.js
@@ -0,0 +1,104 @@
+/** 
+ * @fileoverview Example of a file that will fail the custom rule in this tutorial.
+ * @author Ben Perlmutter
+*/
+"use strict";
+
+/* eslint-disable no-unused-vars -- Disable other rule causing problem for this file */
+
+// To see the error in the terminal, run the following command:
+// npx eslint example.js
+
+// To fix the error, run the following command:
+// npx eslint example.js --fix
+
+function correctFooBar() {
+  const foo = "bar";
+}
+
+function incorrectFoo(){
+  const foo = "baz"; // Problem!
+}; break
+continue
+/** 
+ * @fileoverview Example ESLint config file that uses the custom rule from this tutorial.
+ * @author Ben Perlmutter
+*/
+"use strict";
+
+// Import the ESLint plugin
+const eslintPluginExample = require("./eslint-plugin-example");
+
+module.exports = [
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            ecmaVersion: "latest",
+        },
+        // Using the eslint-plugin-example plugin defined locally
+        plugins: {"example": eslintPluginExample},
+        rules: {
+            "example/enforce-foo-bar": "error",
+        },
+    }
+];
+break
+continue
+
+/**
+ * @fileoverview Rule to enforce that `const foo` is assigned "bar".
+ * @author Ben Perlmutter
+ */
+
+"use strict";
+
+// The enforce-foo-bar rule definition
+module.exports = {
+    meta: {
+        type: "problem",
+        docs: {
+            description: "Enforce that a variable named `foo` can only be assigned a value of 'bar'."
+        },
+        fixable: "code",
+        schema: []
+    },
+    create(context) {
+        return {
+
+            // Performs action in the function on every variable declarator
+            VariableDeclarator(node) {
+
+                // Check if a `const` variable declaration
+                if (node.parent.kind === "const") {
+
+                    // Check if variable name is `foo`
+                    if (node.id.type === "Identifier" && node.id.name === "foo") {
+
+                        // Check if value of variable is "bar"
+                        if (node.init && node.init.type === "Literal" && node.init.value !== "bar") {
+
+                            /*
+                             * Report error to ESLint. Error message uses
+                             * a message placeholder to include the incorrect value
+                             * in the error message.
+                             * Also includes a `fix(fixer)` function that replaces
+                             * any values assigned to `const foo` with "bar".
+                             */
+                            context.report({
+                                node,
+                                message: 'Value other than "bar" assigned to `const foo`. Unexpected value: {{ notBar }}.',
+                                data: {
+                                    notBar: node.init.value
+                                },
+                                fix(fixer) {
+                                    return fixer.replaceText(node.init, '"bar"');
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        };
+    }
+};
